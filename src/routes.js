c95f2a84ce249d94c7bb814a5ba847909e21d512
GuzzMega <guzzt.mega@gmail.com>
2020-05-28 10:45:30 -0300

    Created Booking

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -11,6 +12,7 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
